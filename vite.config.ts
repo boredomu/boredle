45f0583d3b53489bb48ff8f49b7f48b0e01f6e39
boredomu <bennyhe.bh@gmail.com>
2026-06-15 13:56:43 -0400

    Attempt to fix the JSON file path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/music-guessing-game",
+  base: "/music-guessing-game/"
 })
